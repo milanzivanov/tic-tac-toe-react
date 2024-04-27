ebfcc1a3bd97d8baae2e066a26d692836bdcc8e0
milanzivanov <milan.zivanov@gmail.com>
2024-04-27 09:26:32 +0200

    Lifting state up

--- src/App.js
+++ src/App.js
@@ -1,37 +1,69 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X");
-  }
-
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
-  const [squeres, setSquere] = useState(Array(9).fill(null));
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(index) {
+    // const nextSquares = squares.slice();
+    // new way
+    const nextSquares = [...squares];
+    nextSquares[index] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square value={squeres[0]}></Square>
-        <Square value={squeres[1]}></Square>
-        <Square value={squeres[2]}></Square>
+        <Square
+          value={squares[0]}
+          onSquareClick={() => handleClick(0)}
+        ></Square>
+        <Square
+          value={squares[1]}
+          onSquareClick={() => handleClick(1)}
+        ></Square>
+        <Square
+          value={squares[2]}
+          onSquareClick={() => handleClick(2)}
+        ></Square>
       </div>
+
       <div className="board-row">
-        <Square value={squeres[3]}></Square>
-        <Square value={squeres[4]}></Square>
-        <Square value={squeres[5]}></Square>
+        <Square
+          value={squares[3]}
+          onSquareClick={() => handleClick(3)}
+        ></Square>
+        <Square
+          value={squares[4]}
+          onSquareClick={() => handleClick(4)}
+        ></Square>
+        <Square
+          value={squares[5]}
+          onSquareClick={() => handleClick(5)}
+        ></Square>
       </div>
+
       <div className="board-row">
-        <Square value={squeres[6]}></Square>
-        <Square value={squeres[7]}></Square>
-        <Square value={squeres[8]}></Square>
+        <Square
+          value={squares[6]}
+          onSquareClick={() => handleClick(6)}
+        ></Square>
+        <Square
+          value={squares[7]}
+          onSquareClick={() => handleClick(7)}
+        ></Square>
+        <Square
+          value={squares[8]}
+          onSquareClick={() => handleClick(8)}
+        ></Square>
       </div>
     </>
   );

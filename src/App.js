c335a72b4243d1ef4e7712ce440a34c2c41aa82c
milanzivanov <milan.zivanov@gmail.com>
2024-05-29 14:08:48 +0200

    Final cleanup

--- src/App.js
+++ src/App.js
@@ -84,9 +84,9 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
   console.log(currentSquares);
 
@@ -94,25 +94,23 @@ export default function Game() {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
-      description = `Go to move #${move}`;
+      description = `You are at move #${move}`;
     } else {
       description = `Go to game start`;
     }
     // console.log(move);
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        <p onClick={() => jumpTo(move)}>{description}</p>
       </li>
     );
   });
